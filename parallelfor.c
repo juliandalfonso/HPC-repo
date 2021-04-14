23bd51ec99e63a71f5973e37cef1133be7968119
julian <juliandavid.alfonso@utp.edu.co>
2021-04-14 14:38:14 -0500

    nuevo cambio

--- parallelfor.c
+++ parallelfor.c
@@ -6,12 +6,12 @@
 void simple(int n, float[] *a, float[] *b)
 {
  int i;
-#pragma omp parallel for
+#pragma omp parallel num_threads(2) shared(n)
  for (i=1; i<n; i++) /* i is private by default */
  b[i] = (a[i] + a[i-1]) / 2.0;
 }
 
-
+ 
 int main(){
     float a[20], b[20];
 
