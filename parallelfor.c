6965b69e5c34875dad062fd32b662b9c3c64c8cc
julian <juliandavid.alfonso@utp.edu.co>
2021-04-14 14:27:23 -0500

    esto lo hizo vargas

--- parallelfor.c
+++ parallelfor.c
@@ -2,23 +2,22 @@
 #include <omp.h>
 
 
-int main(){
-    float a=1, b=2;
 
-    printf(simple(2, a*, b*));
-    return 0;
-}
-
-
-
-int simple(int n, float *a, float *b)
+void simple(int n, float *a, float *b)
 {
  int i;
 #pragma omp parallel for
  for (i=1; i<n; i++) /* i is private by default */
  b[i] = (a[i] + a[i-1]) / 2.0;
-
- return b[i]
 }
+
+
+int main(){
+    float a[], b[];
+
+    printf(simple(2, &a, &b));
+    return 0;
+}
+
 
 
