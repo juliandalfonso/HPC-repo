987fa8f5296ff23945e981091a287200ef5cce27
julian <juliandavid.alfonso@utp.edu.co>
2021-04-16 14:03:31 -0500

    Codigo con comentarios actualizados

--- ejemplo5.1.c
+++ ejemplo5.1.c
@@ -7,7 +7,7 @@ void subdomain(float *x, int istart, int ipoints)
     //istart -> inicio del array
     int i;
     for (i = 0; i < ipoints; i++)
-        x[istart + i] = 123.456;
+        x[istart + i] = 123.456; //se agrega este numero a todas las posiciones del vector
 }
 
 
@@ -15,15 +15,25 @@ void sub(float *x, int npoints)
 {
     //npoints -> tamano del vector 
 
-    int iam, nt, ipoints, istart;
+    int iam, nt, ipoints, istart; 
+    //iam -> identifica el propio numero de hilo "yo soy"
+
+    //#total de hilos
+
+    //ipoints -> que tanto vamos a partir 
+
+    //istart -> en donde se va a empezar
 #pragma omp parallel default(shared) private(iam, nt, ipoints, istart)
     {
-        iam = omp_get_thread_num();
-        nt = omp_get_num_threads();
+        iam = omp_get_thread_num(); //#identificador de hilo
+        nt = omp_get_num_threads();//#total de hilos
+
         ipoints = npoints / nt; /* size of partition */
         istart = iam * ipoints; /* starting array index */
+        
         if (iam == nt - 1)      /* last thread may do more */
             ipoints = npoints - istart;
+       
         subdomain(x, istart, ipoints);
     }
 }
